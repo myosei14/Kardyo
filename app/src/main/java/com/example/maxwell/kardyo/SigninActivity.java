d2d04931f4812269176df15085874cb86870ac1d
Maxwell Osei Yeboah <myosei14@gmail.com>
2018-02-01 17:55:51 -0800

    Fourth commit

--- app/src/main/java/com/example/maxwell/kardyo/SigninActivity.java
+++ app/src/main/java/com/example/maxwell/kardyo/SigninActivity.java
@@ -1,49 +1,110 @@
 package com.example.maxwell.kardyo;
 
-import android.content.Intent;
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Intent;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class SigninActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-    private EditText mEmail, mPassword;
-    private Button mSigninBtn;
+import org.w3c.dom.Text;
+
+public class SigninActivity extends AppCompatActivity implements View.OnClickListener {
+
+        private EditText mEmail, mPassword;
+        private Button mSigninBtn;
+        private TextView linkToSignup, emailPass;
+        private FirebaseAuth firebaseAuth;
+        private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-        mEmail = (EditText)findViewById(R.id.usernameEt);
-        mPassword = (EditText)findViewById(R.id.passwordEt);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        mEmail = (EditText)findViewById(R.id.siginEmailEt);
+        mPassword = (EditText)findViewById(R.id.siginPassEt);
         mSigninBtn = (Button)findViewById(R.id.siginBtn);
+        linkToSignup = (TextView)findViewById(R.id.linkToSignup);
+        emailPass = (TextView)findViewById(R.id.emailPass);
 
-        final String Email = mEmail.getText().toString().trim();
-        final String Password = mPassword.getText().toString().trim();
 
-        mSigninBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //SignIn_User(Email, Password);
-                if(Email=="youcan@gmail.com" && Password=="youcan"){
-                    Intent main_activity_intent = new Intent(SigninActivity.this, MainActivity.class);
-                    startActivity(main_activity_intent);
-                }
-            }
-        });
+
+        mSigninBtn.setOnClickListener(this);
+        linkToSignup.setOnClickListener(this);
     }
 
-    /*private void SignIn_User(String email, String password) {
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
 
-        if(email==getString(R.string.signinemail)&&password==getString(R.string.signinpassword)){
-            Intent main_activity_intent = new Intent(SigninActivity.this, MainActivity.class);
-            startActivity(main_activity_intent);
-        }
+    @Override
+    public void onClick(View view) {
+            if (view.getId() == R.id.linkToSignup){
+                Intent signup_activity = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(signup_activity);
+            }
 
-    }*/
+            else if (view.getId() == R.id.siginBtn){
+                userSignin();
+            }
+    }
+
+
+    public void userSignin(){
+
+                String myEmail = mEmail.getText().toString();
+                String myPassword = mPassword.getText().toString();
+
+                if (TextUtils.isEmpty(myEmail) || TextUtils.isEmpty(myPassword)){
+                    emailPass.setText("* All fields must be filled");
+                    return;
+                }
+
+                else {
+                    emailPass.setText("");
+                    progressDialog.setMessage("Sigining in ...");
+                    progressDialog.show();
+                    firebaseAuth.signInWithEmailAndPassword(myEmail, myPassword)
+                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        progressDialog.hide();
+                                        Toast.makeText(getApplicationContext(), "Sigin Successful", Toast.LENGTH_SHORT).show();
+                                        directToProfile();
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "Credentials Invalid, Sigin failed !", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }//end of outer else
+
+
+    }//end userSignin method
+
+    public void directToProfile(){
+        Intent main_activity = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(main_activity);
+    }
 
 }
--- app/src/main/java/com/example/maxwell/kardyo/SignupActivity.java
+++ app/src/main/java/com/example/maxwell/kardyo/SignupActivity.java
@@ -1,14 +1,165 @@
 package com.example.maxwell.kardyo;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SignupActivity extends AppCompatActivity {
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import  android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignupActivity extends AppCompatActivity implements OnClickListener {
+
+    private Spinner statusSp, genderSp;
+    private Button signupBtn;
+    private TextView linkToSignin, errTv;
+    private EditText signupNameEt, signupEmailEt, signupContactEt, signupPassEt, signupConPass;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference user_name, user_email, user_password, user_status, user_contact,user_gender;
+    private CheckBox agreementCb;
+    private  String errMessage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        //data posted to database
+        user_name = firebaseDatabase.getReference("username");
+        user_email = firebaseDatabase.getReference("email");
+        user_contact = firebaseDatabase.getReference("contact");
+        user_gender = firebaseDatabase.getReference("gender");
+        user_status = firebaseDatabase.getReference("status");
+        user_password = firebaseDatabase.getReference("password");
+
+        signupNameEt = (EditText)findViewById(R.id.signupNameEt);
+        signupEmailEt = (EditText)findViewById(R.id.signupEmailEt);
+        signupContactEt = (EditText)findViewById(R.id.signupContactEt);
+        signupPassEt = (EditText)findViewById(R.id.signupPassEt);
+        signupConPass = (EditText)findViewById(R.id.signupConPass);
+        errTv = (TextView)findViewById(R.id.errTv);
+        agreementCb = (CheckBox)findViewById(R.id.agreementCb);
+        signupBtn = (Button)findViewById(R.id.signupBtn);
+        statusSp = (Spinner)findViewById(R.id.statusSp);
+        genderSp = (Spinner)findViewById(R.id.genderSp);
+        linkToSignin = (TextView)findViewById(R.id.linkToSignin);
+        progressDialog = new ProgressDialog(this);
+
+        linkToSignin.setOnClickListener(this);
+        signupBtn.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.signupBtn){
+            registerUser();
+        }
+        else if(view.getId() == R.id.linkToSignin){
+            Intent signin_activity = new Intent(getApplicationContext(), SigninActivity.class);
+            startActivity(signin_activity);
+        }
+
+
+    }//end of onclick method
+
+    public void registerUser(){
+
+        String userName = signupNameEt.getText().toString().trim();
+        String userEmail = signupEmailEt.getText().toString().trim();
+        String userPassword = signupPassEt.getText().toString().trim();
+        String userContact = signupContactEt.getText().toString().trim();
+        String userStatus = statusSp.getSelectedItem().toString().trim();
+        String userGender = genderSp.getSelectedItem().toString().trim();
+        String conPass = signupConPass.getText().toString().trim();
+
+        if (TextUtils.isEmpty(userName)|| TextUtils.isEmpty(userEmail)||TextUtils.isEmpty(userContact)|| TextUtils.isEmpty(userPassword)||TextUtils.isEmpty(conPass) || !agreementCb.isChecked() || userContact.length() != 10 || !(userPassword.equals(conPass))){
+
+            if (TextUtils.isEmpty(userName)|| TextUtils.isEmpty(userEmail)||TextUtils.isEmpty(userContact)|| TextUtils.isEmpty(userPassword)||TextUtils.isEmpty(conPass)){
+               errMessage = "* All fields must be filled ";
+               errTv.setText(errMessage);
+                return;
+           }
+
+            if (userContact.length() != 10){
+                errMessage ="* Contact must be 10 ";
+                errTv.setText(errMessage);
+                return;
+            }
+            if (!(userPassword.equals(conPass))){
+                errMessage = "* Passwords donot match ";
+                errTv.setText(errMessage);
+                return;
+            }
+            if (!agreementCb.isChecked()){
+                errMessage = "* Please agree ";
+                errTv.setText(errMessage);
+                return;
+            }
+            return;
+        }
+
+        else {
+            errMessage = "";
+            errTv.setText(errMessage);
+            progressDialog.setMessage("Registering user...");
+            progressDialog.show();
+
+            user_name.setValue(userName);
+            user_email.setValue(userEmail);
+            user_contact.setValue(userContact);
+            user_gender.setValue(userGender);
+            user_status.setValue(userStatus);
+            user_password.setValue(userPassword);
+
+
+            //authentication user for sigin
+            firebaseAuth.createUserWithEmailAndPassword(userEmail, userPassword)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Registration Successful", Toast.LENGTH_SHORT).show();
+                                progressDialog.hide();
+                                directToProfile();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Registration Failed", Toast.LENGTH_SHORT).show();
+                                progressDialog.hide();
+                            }
+                        }
+                    });
+
+        }
+    }//end registerUser
+
+    public void directToProfile(){
+        Intent main_activity = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(main_activity);
     }
 }
